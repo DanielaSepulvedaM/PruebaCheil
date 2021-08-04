74c17ca52d923c8acd1302be5e7dc80d6b9dae49
ingri daniela sepulveda montaña <isepulvedam@ucentral.edu.co>
2021-08-04 00:09:22 -0500

    creo interfaz fotos

---
+++ Entidades/Interfaces/IRepoFotos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades.Interfaces
+{
+    public interface IRepoFotos
+    {
+        IEnumerable<Foto> Listar(int hotelID);
+        Foto Obtener(int fotoId);
+        void Insertar(Foto foto);
+        void Eliminar(int FotoID);
+    }
+}
