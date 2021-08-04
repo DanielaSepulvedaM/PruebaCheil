2fcfd288c69f9c6b05e5af03bc21f95186e1d268
ingri daniela sepulveda montaña <isepulvedam@ucentral.edu.co>
2021-08-04 09:01:20 -0500

    agrego depedencias para documentacion

--- Servicio/Startup.cs
+++ Servicio/Startup.cs
@@ -5,8 +5,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Repositorio;
 using Repositorio.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Servicio
 {
@@ -28,11 +32,33 @@ namespace Servicio
             services.AddScoped<IRepoHotel, RepoHotel>();
             services.AddScoped<IRepoFotos, RepoFotos>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Hotel API",
+                    Description = "Sercicio Rest para hotel"
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotel API V1");
+                c.RoutePrefix = string.Empty;
+            });     
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
