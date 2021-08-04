4f9fee907d505297b5fd6ad29387481da0ee9328
ingri daniela sepulveda montaña <isepulvedam@ucentral.edu.co>
2021-08-04 09:00:04 -0500

    agrego validacion a las propiedades

--- Entidades/Hotel.cs
+++ Entidades/Hotel.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entidades
 {
     public class Hotel
     {
+       
         public int HotelID { get; set; }
+
+        [Required(ErrorMessage = "Introduzca el nombre del hotel")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Elegir la categoria 1 a 5 estrellas")]
         public int Categoria { get; set; }
+
+        [Required(ErrorMessage = "Elegir el precio del hotel")]
         public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "agregar fotos")]
         public string Foto { get; set; }
+
         public double Calificacion { get; set; }
     }
 }
